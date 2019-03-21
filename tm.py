d6f4706c4a7cc7961c7fa98462f425b8bbe2e0d2
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-21 21:56:18 +0900

    'm'

--- tm.py
+++ tm.py
@@ -1,6 +1,6 @@
 import telebot
 bot=telebot.TeleBot("354151340:AAEnLGjyv9CEl1TAwNkimzOCVsyI-P_2oZM")
-f = open('text.txt')
+f = open('text.txt',"r")
 keys={}
 for line in f:
     arr=line.split("$$")
