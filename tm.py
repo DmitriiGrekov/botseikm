35b7ff5422e99f98aa2248d49e0a5737d4449c99
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-21 21:37:32 +0900

    'm'

---
+++ tm.py
@@ -0,0 +1,23 @@
+import telebot
+bot=telebot.Telebot("354151340:AAEnLGjyv9CEl1TAwNkimzOCVsyI-P_2oZM")
+f = open('text.txt')
+keys={}
+for line in f:
+    arr=line.split("$$")
+    keys[arr[0]]=arr[-1]
+    
+@bot.message_handler(commands=["start"])
+def send_message2(message):
+    bot.send_message(message.chat.id,"Введите вопрос")
+
+@bot.message_handler(content_types=["text"])
+def mes_handler(message):
+    for i in keys.keys():
+        questions=message.chat.id
+        if questions == i:
+            bot.send_message(message.chat.id,"Ваш ответ - "+ keys[i])
+        else:
+            bot.send_message(message.chat.id,'Вопрос не найден')
+            
+    
+    
