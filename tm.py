5c1bd262ccf5039053c157fc2427af0961595b4b
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-30 22:09:10 +0900

    news

--- tm.py
+++ tm.py
@@ -3,3 +3,6 @@ bot = telebot.TeleBot("354151340:AAEnLGjyv9CEl1TAwNkimzOCVsyI-P_2oZM")
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     bot.send_message(message.chat.id,"Enter the questions.")
+if __name__ == "__main__":
+    
+    bot.polling()
